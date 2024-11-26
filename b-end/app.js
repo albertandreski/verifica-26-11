746f58b8cbadd47e1f48747f33e510efb2b1c53d
albertandreski <andreski.albert@iisgalvanimi.edu.it>
2024-11-26 12:18:42 +0000

    punto 5 a metà

---
+++ b-end/app.js
@@ -0,0 +1,17 @@
+// app.js
+const express = require('express');
+const app = express();
+const port = 3000;
+
+
+app.use(express.static('www'));
+
+
+app.get('/api/mante', (req, res) => {
+  res.json(require('./mante.json'));
+});
+
+
+app.listen(port, () => {
+  console.log(`Server in ascolto sulla porta ${port}`);
+});
